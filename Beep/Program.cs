a04b9a9427b395b1ff04b956c73104c22de3f098
Roman Blinkov <roman@blinkov.com>
2019-09-28 18:59:05 +0300

    Add project files.

---
+++ Beep/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Beep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Beep();
+            Console.ReadLine();
+        }
+    }
+}
