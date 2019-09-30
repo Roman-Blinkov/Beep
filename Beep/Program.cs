e3ea717c237378550a6cfb6f6b06de8b04f00916
Roman Blinkov <roman@blinkov.com>
2019-09-30 07:57:00 +0300

    [+] Added a bit of code

--- Beep/Program.cs
+++ Beep/Program.cs
@@ -6,7 +6,13 @@ namespace Beep
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Beep with arguments. Press enter");
+            Console.ReadLine();
+            Console.Beep(500, 300);
+            Console.WriteLine("Beep without arguments. Press enter");
+            Console.ReadLine();
             Console.Beep();
+            Console.WriteLine("Beep finished. Press enter to exit");
             Console.ReadLine();
         }
     }
